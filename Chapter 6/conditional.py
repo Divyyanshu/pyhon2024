02124556e5e9a0a3669b879a19a4f92d2e2bee83
divyanshu tailor <128120812+divyyuanshutailor@users.noreply.github.com>
2024-06-13 07:29:15 +0530

    Your commit message

---
+++ Chapter 6/conditional.py
@@ -0,0 +1,8 @@
+age = int(input("enter your age is : "))
+
+if(age >= 18):
+    print("your is 18 plus")
+else:
+    print('your are not 18 plus underage human')
+
+print("end of program")
---
+++ Chapter 6/if_else_elif_ladder.py
@@ -0,0 +1,13 @@
+age = int(input("enter your age is : "))
+
+if(age >= 18):
+    print("your is 18 plus")
+
+elif(age<0):
+    print("invlaid age its negetive value entered")
+elif(age==0):
+    print('enter 0 invalid age ')
+else:
+    print('your are not 18 plus underage human')
+
+print("end of program")
