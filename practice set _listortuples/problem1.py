59fb5205c3f3e0edd033ea3b74776f8393928b57
divyanshu tailor <128120812+divyyuanshutailor@users.noreply.github.com>
2024-06-18 17:47:37 +0530

    chapter4

---
+++ practice set _listortuples/problem1.py
@@ -0,0 +1,22 @@
+fruits = []
+
+# input user
+
+f1 = input("enter your fruits : ")
+fruits.append(f1)
+f2 = input("enter your fruits : ")
+fruits.append(f2)
+f3 = input("enter your fruits : ")
+fruits.append(f3)
+f4 = input("enter your fruits : ")
+fruits.append(f4)
+f5 = input("enter your fruits : ")
+fruits.append(f5)
+f6 = input("enter your fruits : ")
+fruits.append(f6)
+f7 = input("enter your fruits : ")
+fruits.append(f7)
+f8 = input("enter your fruits : ")
+fruits.append(f8)
+
+print(fruits)
---
+++ practice set _listortuples/problem2.py
@@ -0,0 +1,19 @@
+MARKS = []
+
+# input user
+# convert firstly in integer
+S1 = int(input("enter your marks here : "))
+MARKS.append(S1)
+S2 = int(input("enter your marks here : "))
+MARKS.append(S2)
+S3 = int(input("enter your marks here : "))
+MARKS.append(S3)
+S4 = int(input("enter your marks here : "))
+MARKS.append(S4)
+S5 = int(input("enter your marks here : "))
+MARKS.append(S5)
+S6 = int(input("enter your marks here : "))
+MARKS.append(S6)
+
+MARKS.sort()
+print(MARKS)
