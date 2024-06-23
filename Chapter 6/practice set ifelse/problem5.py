2fc364cf09b52dc5d7d3e281803d72214b427244
divyanshu tailor <128120812+divyyuanshutailor@users.noreply.github.com>
2024-06-23 17:28:34 +0530

    partice question

---
+++ Chapter 6/practice set ifelse/problem5.py
@@ -0,0 +1,6 @@
+username = input("Enter your name : ")
+
+if(len(username)<=10):
+    print("less words pls above 10 char")
+else:
+    print("well done")
---
+++ Chapter 6/practice set ifelse/problem6.py
@@ -0,0 +1,8 @@
+l = ["divyanshu" , "dhwani" , "mayank" , "sahaj","mummy","papa"]
+
+name = input("Enter your name : ")
+
+if(name in l):
+    print("valid name ")
+else:
+    print("no valid ")
