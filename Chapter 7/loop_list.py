7ac729579277ba01894f55de9a91bc4619b40a91
divyanshu tailor <128120812+divyyuanshutailor@users.noreply.github.com>
2024-06-24 19:28:29 +0530

    loop methods way

---
+++ Chapter 7/loop_list.py
@@ -0,0 +1,6 @@
+l = [1 , "divyanshu" , "hello" , 654]
+i = 0
+
+while (i<len(l)):
+    print(i)
+    i = i + 1
---
+++ Chapter 7/multi_loop.py
@@ -0,0 +1,9 @@
+t = {1 , "dt" , "kk " , "ramji " , 542155 , False}
+
+for  i in t :
+    print(i)
+
+l = ["jk" ,"cement" , True , 55454]
+
+for i in l:
+    print(i)
