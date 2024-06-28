40cf7b2dee26891ea73fa08be405fcb5e5646c5d
divyanshu tailor <128120812+divyyuanshutailor@users.noreply.github.com>
2024-06-28 10:37:30 +0530

    functions def

---
+++ chapter 8/function.py
@@ -0,0 +1,10 @@
+def avg():
+    a = int(input("enter your number a : "))
+    b = int(input("enter your number b : "))
+    c = int(input("enter your number c : "))
+
+    avrage = (a + b + c )/3
+    print(avrage)
+
+
+avg()
---
+++ chapter 8/quick_qui.py
@@ -0,0 +1,10 @@
+def goodby(name , ending):
+    print("Good bye ,", name)
+    print(ending)
+    return "breakup done!"
+
+a = goodby("dhwani", "thankyou")
+# goodby("ankit ", "thankyou")
+# goodby("harsh", "thankyou")
+# goodby("pratham", "thankyou")
+print(a)
